a94bc00c50fd322fa307e8a6029d28ac63187b9c
devsh0 <devashishjaiswal86@gmail.com>
2022-07-28 14:08:43 +0530

    Extend JpaRepository instead of CrudRepository

--- src/main/java/com/devsh0/chirp/repository/UserRepository.java
+++ src/main/java/com/devsh0/chirp/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.devsh0.chirp.repository;
 
 import com.devsh0.chirp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select user from User user where user.email=:email")
     Optional<User> findUserByEmail(String email);
     @Query("select user from User user where user.username=:username")
