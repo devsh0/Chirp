cbb25bbd11ec94f45d817ae7b389335bacfbae03
devsh0 <devashishjaiswal86@gmail.com>
2022-07-28 12:11:58 +0530

    Provide a custom builder for User entity
    The automatically generated lombok builder didn't help.

--- src/main/java/com/devsh0/chirp/entity/User.java
+++ src/main/java/com/devsh0/chirp/entity/User.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 @Getter
 @Setter
 @ToString
-@Builder
 @Table
 public class User {
     @Id
@@ -18,7 +17,29 @@ public class User {
     private String username;
     private String password;
 
-    public User() {
+    public static class Builder {
+        private final User user = new User();
 
+        public Builder email(String email) {
+            user.setEmail(email);
+            return this;
+        }
+
+        public Builder username(String username) {
+            user.setUsername(username);
+            return this;
+        }
+
+        public Builder password(String password) {
+            user.setPassword(password);
+            return this;
+        }
+
+        public User build() {
+            return user;
+        }
+    }
+    public static Builder builder() {
+        return new Builder();
     }
 }
