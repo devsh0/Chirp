c1171949068cbfc0a56ccecfbe86ca167eef8e56
devsh0 <devashishjaiswal86@gmail.com>
2022-07-29 12:57:25 +0530

    Mark activateUser Transactional and Modifying
    Also ensure that the update query to activate a user is flushed and
    reflected immediately with clearAutomatically = true.

--- src/main/java/com/devsh0/chirp/repository/UserRepository.java
+++ src/main/java/com/devsh0/chirp/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.devsh0.chirp.repository;
 
 import com.devsh0.chirp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
     @Query("select user from User user where user.username=:username")
     Optional<User> findUserByUsername(String username);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update User u set u.active=1 where u.id=:userId")
+    void activateUser(Long userId);
 }
