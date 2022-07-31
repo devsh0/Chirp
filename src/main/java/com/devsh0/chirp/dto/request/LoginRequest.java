cb149800ab960257b8d74480828a330895cfa1aa
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 17:59:09 +0530

    LoginRequest: mark all the fields @NotBlank

--- src/main/java/com/devsh0/chirp/dto/request/LoginRequest.java
+++ src/main/java/com/devsh0/chirp/dto/request/LoginRequest.java
@@ -2,9 +2,13 @@ package com.devsh0.chirp.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class LoginRequest {
+    @NotBlank(message = "username or email is required!")
     private String user;
+    @NotBlank(message = "password is required!")
     private String password;
 
     public LoginRequest(String user, String password) {
