8d58ad3dd0a3c099451aa9db37c8f5874d8f8e35
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 18:05:29 +0530

    Clean up registration tests and add a few

--- src/test/java/com/devsh0/chirp/controller/AuthenticationControllerTest.java
+++ src/test/java/com/devsh0/chirp/controller/AuthenticationControllerTest.java
@@ -1,8 +1,5 @@
 package com.devsh0.chirp.controller;
 
-import com.devsh0.chirp.dto.request.RegistrationRequest;
-import com.devsh0.chirp.dto.response.RegistrationResponse;
-import com.devsh0.chirp.entity.User;
 import com.devsh0.chirp.service.AuthenticationService;
 import com.devsh0.chirp.util.Utils;
 import org.junit.jupiter.api.Test;
@@ -16,7 +13,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
+import static org.springframework.mock.web.MockHttpServletResponse.SC_BAD_REQUEST;
+import static org.springframework.mock.web.MockHttpServletResponse.SC_OK;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
@@ -33,45 +32,52 @@ class AuthenticationControllerTest {
     @MockBean
     private AuthenticationService authService;
 
-    private MockHttpServletResponse registrationRequestHelper(RegistrationRequest requestStub) throws Exception {
-        return this.mockMvc.perform(post("/api/v1/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(Utils.asJson(requestStub)))
-                .andReturn().getResponse();
+    @Test
+    public void registrationSucceedsOnValidInput() throws Exception {
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", email).put("username", username).put("password", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
     }
 
     @Test
-    public void registrationSucceedsOnValidInput() throws Exception {
-        var validUser = User.builder().email(email).username(username).password(password).build();
-        when(authService.register(email, username, password)).thenReturn(validUser);
-        var request = new RegistrationRequest(email, username, password);
-        var response = registrationRequestHelper(request);
-        var responseBody = Utils.fromJson(response.getContentAsString(), RegistrationResponse.class);
-        var expectedResponseBody = new RegistrationResponse().setSuccess(true).setError(Utils.emptyMap());
-        assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_OK);
-        assertThat(responseBody).isEqualTo(expectedResponseBody);
-
+    public void registrationFailsOnMissingEmail() throws Exception {
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("username", username).put("password", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
     }
 
     @Test
     public void registrationFailsOnInvalidEmail() throws Exception {
-        var request = new RegistrationRequest("test", username, password);
-        var response = registrationRequestHelper(request);
-        var responseBody = Utils.fromJson(response.getContentAsString(), RegistrationResponse.class);
-        var expectedResponseBody = new RegistrationResponse().setSuccess(false).setError(Utils.mapFrom("email", "invalid email!").get());
-        assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_BAD_REQUEST);
-        assertThat(responseBody).isEqualTo(expectedResponseBody);
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", "blah").put("username", username).put("password", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void registrationFailsOnMissingUsername() throws Exception {
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", email).put("password", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
     }
 
     @Test
     public void registrationFailsOnInvalidUsername() throws Exception {
-        var request = new RegistrationRequest(email, "us", password);
-        var response = registrationRequestHelper(request);
-        var responseBody = Utils.fromJson(response.getContentAsString(), RegistrationResponse.class);
-        var errorMap = Utils.mapFrom("username", "username must be between 3 to 30 characters long!").get();
-        var expectedResponseBody = new RegistrationResponse().setSuccess(false).setError(errorMap);
-        assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_BAD_REQUEST);
-        assertThat(responseBody).isEqualTo(expectedResponseBody);
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", email).put("username", "ab").put("password", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void registrationFailsOnMissingPassword() throws Exception {
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", email).put("username", username).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
     }
 
     @Test
