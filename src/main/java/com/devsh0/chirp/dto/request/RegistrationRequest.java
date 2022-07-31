686b0a39afa7c99fd32d58f95fea849dcdb89fc4
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 18:00:40 +0530

    Mark all the fields @NotBlank

--- src/main/java/com/devsh0/chirp/dto/request/RegistrationRequest.java
+++ src/main/java/com/devsh0/chirp/dto/request/RegistrationRequest.java
@@ -3,16 +3,20 @@ package com.devsh0.chirp.dto.request;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 public class RegistrationRequest {
+    @NotBlank(message = "email is required!")
     @Email(message = "invalid email!")
     private final String email;
 
+    @NotBlank(message = "username is required!")
     @Size(min = 3, max = 30, message = "username must be between 3 to 30 characters long!")
     private final String username;
 
+    @NotBlank(message = "password is required!")
     @Size(min = 8, message = "password must be at least 8 characters long!")
     private final String password;
 }
