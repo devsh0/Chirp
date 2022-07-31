1bcbefe3cfe67bf1523f79ed3e41d3f479b03fb9
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 18:00:00 +0530

    Remove @NotNull from fields, @NotBlank is sufficient

--- src/main/java/com/devsh0/chirp/dto/request/PasswordResetRequest.java
+++ src/main/java/com/devsh0/chirp/dto/request/PasswordResetRequest.java
@@ -2,19 +2,17 @@ package com.devsh0.chirp.dto.request;
 
 import lombok.Data;
 import lombok.Getter;
+
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @Getter
 public class PasswordResetRequest {
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "old password is required!")
     private String oldPassword;
 
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "new password is required!")
     @Size(min = 8, message = "Password must be at least 8 characters long!")
     private String newPassword;
 
