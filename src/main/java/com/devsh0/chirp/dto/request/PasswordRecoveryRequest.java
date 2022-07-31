5f8127e62824978004e93cbf88f202dc26483617
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 17:58:21 +0530

    PasswordRecoveryRequest: mark email @NotBlank

--- src/main/java/com/devsh0/chirp/dto/request/PasswordRecoveryRequest.java
+++ src/main/java/com/devsh0/chirp/dto/request/PasswordRecoveryRequest.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Getter
 public class PasswordRecoveryRequest {
+    @NotBlank(message = "email is required!")
     @Email(message = "invalid email!")
     private String email;
 }
