2d3fb647a3d0fb8a38d830c1b98067c67fc72fa1
devsh0 <devashishjaiswal86@gmail.com>
2022-07-31 18:05:43 +0530

    Add a bunch of controller tests

--- src/test/java/com/devsh0/chirp/controller/AuthenticationControllerTest.java
+++ src/test/java/com/devsh0/chirp/controller/AuthenticationControllerTest.java
@@ -82,12 +82,185 @@ class AuthenticationControllerTest {
 
     @Test
     public void registrationFailsOnInvalidPassword() throws Exception {
-        var request = new RegistrationRequest(email, username, "pass");
-        var response = registrationRequestHelper(request);
-        var responseBody = Utils.fromJson(response.getContentAsString(), RegistrationResponse.class);
-        var errorMap = Utils.mapFrom("password", "password must be at least 8 characters long!").get();
-        var expectedResponseBody = new RegistrationResponse().setSuccess(false).setError(errorMap);
-        assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_BAD_REQUEST);
-        assertThat(responseBody).isEqualTo(expectedResponseBody);
+        String path = "/api/v1/auth/register";
+        var request = Utils.mapFrom("email", email).put("username", username).put("password", "blah").asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(request)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void tokenVerificationFailsOnMissingTokenParam() throws Exception {
+        String path = "/api/v1/auth/verify";
+        var response = mockMvc.perform(get(path)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void tokenVerificationSucceedsOnValidTokenParam() throws Exception {
+        String path = "/api/v1/auth/verify";
+        var response = mockMvc.perform(get(path).param("token", "")).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_OK);
+    }
+
+    @Test
+    public void loginFailsOnMissingUserField() throws Exception {
+        String path = "/api/v1/auth/login";
+        var requestBody = Utils.mapFrom("password", "pass").asJson();
+        var response = mockMvc.perform(post(path).content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void loginFailsOnMissingPasswordField() throws Exception {
+        String path = "/api/v1/auth/login";
+        var requestBody = Utils.mapFrom("user", "user").asJson();
+        var response = mockMvc.perform(post(path).content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void loginFailsOnEmptyUser() throws Exception {
+        String path = "/api/v1/auth/login";
+        var requestBody = Utils.mapFrom("user", "").put("password", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void loginFailsOnEmptyPassword() throws Exception {
+        String path = "/api/v1/auth/login";
+        var requestBody = Utils.mapFrom("user", username).put("password", "").asJson();
+        var response = mockMvc.perform(post(path).content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void loginSucceedsOnValidInput() throws Exception {
+        String path = "/api/v1/auth/login";
+        var requestBody = Utils.mapFrom("user", username).put("password", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    public void passwordResetFailsOnMissingAuthorizationHeader() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("oldPassword", password).put("newPassword", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void passwordResetFailsOnMissingOldPassword() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("newPassword", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void passwordResetFailsOnMissingNewPassword() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("oldPassword", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void passwordResetFailsOnEmptyOldPassword() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("oldPassword", "").put("newPassword", password).asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void passwordResetFailsOnEmptyNewPassword() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("oldPassword", password).put("newPassword", "").asJson();
+        var response = mockMvc.perform(post(path).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void passwordResetSucceedsOnValidInput() throws Exception {
+        String path = "/api/v1/auth/reset-password";
+        var requestBody = Utils.mapFrom("newPassword", password).put("oldPassword", password).asJson();
+        var response = mockMvc.perform(post(path).header("Authorization", "")
+                .content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    public void logoutFailsOnMissingAuthorizationHeader() throws Exception {
+        String path = "/api/v1/auth/logout";
+        var response = mockMvc.perform(post(path)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void logoutSucceedsWhenAuthorizationHeaderIsPresent() throws Exception {
+        String path = "/api/v1/auth/logout";
+        var response = mockMvc.perform(post(path).header("Authorization", "")).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    void passwordRecoveryFailsOnMissingEmail() throws Exception {
+        String path = "/api/v1/auth/recover-password";
+        var response = mockMvc.perform(get(path)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void passwordRecoveryFailsOnInvalidEmail() throws Exception {
+        String path = "/api/v1/auth/recover-password";
+        var requestBody = Utils.mapFrom("email", "invalid#email.com").asJson();
+        var response = mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void passwordRecoverySucceedsOnValidEmail() throws Exception {
+        String path = "/api/v1/auth/recover-password";
+        var requestBody = Utils.mapFrom("email", email).asJson();
+        var response = mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    void createNewPasswordFailsOnMissingPassword() throws Exception {
+        String path = "/api/v1/auth/create-new-password";
+        var requestBody = Utils.mapFrom("token", "abc").asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void createNewPasswordFailsOnMissingToken() throws Exception {
+        String path = "/api/v1/auth/create-new-password";
+        var requestBody = Utils.mapFrom("newPassword", password).asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void createNewPasswordFailsOnInvalidPassword() throws Exception {
+        String path = "/api/v1/auth/create-new-password";
+        var requestBody = Utils.mapFrom("newPassword", "sevenn7").put("token", "").asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_BAD_REQUEST);
+    }
+
+    @Test
+    void createNewPasswordSucceedsOnValidInput() throws Exception {
+        String path = "/api/v1/auth/create-new-password";
+        var requestBody = Utils.mapFrom("newPassword", password).put("token", "").asJson();
+        var response = mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
     }
 }
