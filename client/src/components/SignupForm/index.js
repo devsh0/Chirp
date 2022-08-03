f51090ad221fee0a9492fa5ff3caf1bfd5f1eb46
devsh0 <devashishjaiswal86@gmail.com>
2022-08-03 13:50:10 +0530

    Split front page header text to two lines

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -5,7 +5,9 @@ import PasswordField from "../PasswordField";
 
 export default function SignupForm() {
   return (
-    <div className={"bg-dark-secondary m-auto sm:w-1/2 text-sm rounded font-normal p-8"}>
+    <div
+      className={"border w-10/12 bg-dark-secondary m-auto sm:w-1/2 text-sm rounded font-normal p-8"}
+    >
       <form className={"flex flex-col space-y-2"}>
         <EmailField />
         <UsernameField />
@@ -13,7 +15,12 @@ export default function SignupForm() {
         <SignupButton />
       </form>
       <p className={"mt-2 text-sm"}>
-        or <button className={"underline text-blue-twitter font-bold"}>Log In</button>
+        or{" "}
+        <button
+          className={"transition-colors hover:text-blue-twitter-dark text-blue-twitter font-bold"}
+        >
+          Log In
+        </button>
       </p>
     </div>
   );
