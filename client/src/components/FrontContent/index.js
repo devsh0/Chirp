da0ef894463a70addfdd0e795711552b76dfb538
devsh0 <devashishjaiswal86@gmail.com>
2022-08-03 13:49:39 +0530

    Full width to FrontContent on smaller devices

--- client/src/components/FrontContent/index.js
+++ client/src/components/FrontContent/index.js
@@ -3,14 +3,13 @@ import imgChirpLogo from "../../chirp-logo.png";
 
 export default function FrontContent() {
   return (
-    <main
-      className={"w-10/12 lg:w-1/2 flex flex-col justify-center py-4 text-center text-gray-100"}
-    >
+    <main className={"w-full lg:w-1/2 flex flex-col justify-center py-4 text-center text-gray-100"}>
       <figure>
         <img className={"m-auto sm:w-20 w-12 sm:w-20"} src={imgChirpLogo} alt={"chirp logo"} />
       </figure>
       <header className={"text-2xl mb-5 sm:text-3xl font-bold"}>
-        Stay in the loop! Join Chirp and find out what everyone's up to.
+        Stay in the loop! <br />
+        Join Chirp and find out what everyone's up to.
       </header>
       <SignupForm />
     </main>
