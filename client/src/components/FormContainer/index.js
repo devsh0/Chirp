ac6e500e01dced4816389b3f461d9c29d31ff80f
devsh0 <devashishjaiswal86@gmail.com>
2022-08-04 12:35:04 +0530

    Squish form elements slightly.
    This is to prevent the footer going out-of-page in android chrome. Not
    sure what's up with that.

--- client/src/components/FormContainer/index.js
+++ client/src/components/FormContainer/index.js
@@ -1,9 +1,7 @@
 export default function FormContainer(props) {
   return (
     <div
-      className={
-        "transition-all border w-10/12 bg-dark-secondary m-auto sm:w-1/2 text-sm rounded font-normal p-8"
-      }
+      className={"border w-10/12 bg-dark-secondary m-auto sm:w-1/2 text-sm rounded font-normal p-4"}
     >
       {props.children}
     </div>
--- client/src/components/SignupButton/index.js
+++ client/src/components/SignupButton/index.js
@@ -5,7 +5,7 @@ export default function ActionButton({ btnText, onSubmit }) {
         href={"#"}
         onClick={onSubmit}
         className={
-          "mt-4 text-center inline-block bg-blue-twitter hover:bg-blue-twitter-dark transition-colors w-full py-2 rounded-sm bg-blue-twitter"
+          "mt-2 text-center inline-block bg-blue-twitter hover:bg-blue-twitter-dark transition-colors w-full py-2 rounded-sm bg-blue-twitter"
         }
       >
         {btnText}
